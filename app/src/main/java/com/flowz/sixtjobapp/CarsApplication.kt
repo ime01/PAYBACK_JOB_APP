8d1e3df7edea3821779a4b035bc34838b42e7c63
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-09-29 21:12:45 -0700

    Sixt App

---
+++ app/src/main/java/com/flowz/sixtjobapp/CarsApplication.kt
@@ -0,0 +1,9 @@
+package com.flowz.sixtjobapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class CarsApplication:Application() {
+}
---
+++ app/src/main/java/com/flowz/sixtjobapp/model/Cars.kt
@@ -0,0 +1,5 @@
+package com.flowz.sixtjobapp.model
+
+
+data class Cars(val cars: List<Car>)
+//class Cars : ArrayList<Car>()
